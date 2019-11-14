6c76b7c337eab95e39fbc5c9d95c1f784c96945b
Jordan Luong <jordanw.luong@gmail.com>
2019-11-14 14:48:46 +0900

    bug fixes - edit user

--- app/graphql/mutations/edit_user.rb
+++ app/graphql/mutations/edit_user.rb
@@ -1,14 +1,17 @@
 module Mutations
-  class CreateUser < BaseMutation
+  class EditUser < BaseMutation
     argument :user_args, Types::UserArgs, required: true
 
     type Types::UserType
 
     def resolve(user_args: nil)
+      logged_in_check
+
       context[:current_user].update!(
         user_args.to_h
       )
 
+      context[:current_user]
     rescue ActiveRecord::RecordInvalid => e
       GraphQL::ExecutionError.new("Invalid input: #{e.record.errors.full_messages.join(', ')}")
     end

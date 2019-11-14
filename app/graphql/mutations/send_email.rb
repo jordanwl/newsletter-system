61f7e4f5669acaeed5c46506b110fb54407114ca
Jordan Luong <jordanw.luong@gmail.com>
2019-11-14 12:00:24 +0900

    finish email sent

--- app/graphql/mutations/send_email.rb
+++ app/graphql/mutations/send_email.rb
@@ -1,19 +1,36 @@
 module Mutations
   class SendEmail < BaseMutation
-    description "sends an email to all newsletter subscribers given an email_id"
-    argument :email_id, ID, required: true
+    description "sends an email to all newsletter subscribers given an email_id or to all subscribers given content"
 
-    field :email, Types::EmailType, null: false
+    argument :email_id, ID, required: false
+    argument :content, String, required: false
+
     field :emails_sent, String, null: false
 
-    def resolve(email_id: nil)
-      email = Email.find_by(id: email_id)
+    def resolve(email_id: nil, content: nil)
+      logged_in_check
 
-      return GraphQL::ExecutionError.new("invalid email (check if email exists and if it is your email)") if email.nil? || !email_ownership_check(email_id)
+      case
+      when !email_id.nil? && !content.nil?
+        return GraphQL::ExecutionError.new("enter either an email id or content")
 
-      send_email(email)
+      when !email_id.nil?
+        email = Email.find_by(id: email_id)
 
-      { email: email, emails_sent: @@emails_sent}
+        return GraphQL::ExecutionError.new("invalid email (check if email exists and if it is your email)") if email.nil? || !email_ownership_check(email_id)
+
+        send_email(email)
+
+        { emails_sent: @@emails_sent}
+
+      when !content.nil?
+        send_custom_email(content)
+
+        { emails_sent: @@emails_sent }
+
+      else
+        return GraphQL::ExecutionError.new("enter either an email id or content")
+      end
     end
 
     def email_ownership_check(email_id)
@@ -24,8 +41,12 @@ module Mutations
       email_sent_counter = 0
       email_recipients = []
 
+      return GraphQL::ExecutionError.new("newsletter has no subscribers") if email.newsletter.subscribers.empty?
+
       email.newsletter.subscribers.each do |subscriber|
-        puts "#{subscriber.name}, #{email.content}"
+        puts "To: #{subscriber.name},"
+        puts "#{email.content}"
+
         email_recipients << subscriber.name
         email_sent_counter += 1
       end
@@ -33,5 +54,26 @@ module Mutations
       @@emails_sent = "#{email_sent_counter} email(s) sent (#{email_recipients.join(', ')})"
     end
 
+    def send_custom_email(content)
+      email_sent_counter = 0
+      subscribers = []
+
+      context[:current_user].newsletters.each do |newsletter|
+        subscribers << newsletter.subscribers
+      end
+
+      return GraphQL::ExecutionError.new("you have no subscribers") if subscribers.empty?
+
+      subscribers.flatten!.uniq!
+
+      subscribers.each do |subscriber|
+        puts "To: #{subscriber.name},"
+        puts "#{content}"
+
+        email_sent_counter += 1
+      end
+
+      @@emails_sent = "#{email_sent_counter} email(s) sent to your subscribers (#{subscribers.pluck(:name).join(', ')})"
+    end
   end
 end
